51797cccd0ad7520e6bd16d85431b9ec55100613
huysam93 <huysam93@gmail.com>
2025-06-20 08:46:28 +0700

    add dist

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+import path from 'path';
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
