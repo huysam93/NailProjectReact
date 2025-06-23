0b0cb5481b3d9b36077102e024edca24c7aeaf3a
huysam93 <huysam93@gmail.com>
2025-06-23 15:23:56 +0700

    api

--- backend/routes/visitors.js
+++ backend/routes/visitors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const visitorsController = require('../controllers/visitors_controller');
 
-router.post('/visitors', visitorsController.recordVisit);
-router.get('/visitors/stats', visitorsController.getVisitorStats);
+router.post('api/visitors', visitorsController.recordVisit);
+router.get('api/visitors/stats', visitorsController.getVisitorStats);
 
 module.exports = router;
